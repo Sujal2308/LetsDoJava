c6eed798bb7c491aaf997935c7edcfbd0201ee21
Sujal <sujalbhugul08@gmail.com>
2024-03-05 14:21:58 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+        byte num2 = 2;
+        char c;
+        System.out.println("my number is "+num1);
+        System.out.println("My number is "+num2);
+        c = 'h';
+        String name ;
+        System.out.println(c);
+        name = "sujal bhugul";
+        System.out.println(name);
+        System.out.println("Hello \'world\' ");
+        System.out.println("hello \n \bWorld");
+        System.out.println("*");
+        System.out.println("**");
+
+    }
+}
